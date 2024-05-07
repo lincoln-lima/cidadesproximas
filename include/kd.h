77193f8b5559ea5fba33bfc441b16a5d841a4a4a
Lincoln Lima <lincoln.lima@ufms.br>
2024-05-07 00:22:30 -0400

    padronizando estrutura

--- include/kd.h
+++ include/kd.h
@@ -11,23 +11,22 @@ typedef struct {
 	int dim;
 	double (* dist)(void *, void *);
 	double (* cmp)(void *, void *, int);
-	void (* printa)(void *);
-	int (* get_key)(void *);
+	void (* exibe)(void *);
 } Arv;
 
-void constroi_kd(Arv * arv, int dim, double (* dist)(void *, void *), double (* cmp)(void *, void *, int), void (* printa)(void *), int (* get_key)(void *));
+void constroi_kd(Arv * arv, int dim, double (* dist)(void *, void *), double (* cmp)(void *, void *, int), void (* exibe)(void *));
 void insere_kd(Arv * arv, void * reg);
 void insere_node_kd(Arv * arv, Node ** node, void * reg, int eixo);
-void * busca_kd(Arv * arv, void * reg, int cod);
-void * busca_node_kd(Arv * arv, Node * node, void * reg, int eixo, int cod);
+void * busca_kd(Arv * arv, void * reg);
+void * busca_node_kd(Arv * arv, Node * node, void * reg, int eixo);
 //int remove_kd(Arv * arv, void * reg);
 //int remove_node_kd(Arv * arv, Node * node, void * reg);
 void libera_kd(Arv * arv);
 void libera_node_kd(Node * node);
-void printa_kd(Arv * arv);
-void printa_node_kd(Arv * arv, Node * node);
+void exibe_kd(Arv * arv);
+void exibe_node_kd(Arv * arv, Node * node);
 int conta_kd(Arv * arv);
 int conta_node_kd(Node * node);
-void atribui_distancias(Arv * arv, Node * node, Node * aux, float * distancias, int tam, int * i); 
-void n_proximos_kd(Arv * arv, Node * node, int n); 
+void atribui_distancias(Arv * arv, void * reg, Node * node, float * distancias, int tam, int * i); 
+void n_proximos_kd(Arv * arv, void * reg, int n); 
 #endif
