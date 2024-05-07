85a58420f2343a5f033ca0c3e9e7becd807076e6
Lincoln Lima <lincoln.lima@ufms.br>
2024-05-07 00:24:45 -0400

    padronizando estrutura, funções e avanço na construção da função de cálculo de distância

--- src/kd.c
+++ src/kd.c
@@ -1,12 +1,11 @@
 #include "../include/kd.h"
 
-void constroi_kd(Arv * arv, int dim, double (* dist)(void *, void *), double (* cmp)(void *, void *, int eixo), void (* printa)(void *), int (* get_key)(void *)) {
+void constroi_kd(Arv * arv, int dim, double (* dist)(void *, void *), double (* cmp)(void *, void *, int eixo), void (* exibe)(void *)) {
     arv->raiz = NULL;
     arv->dim = dim;
     arv->dist = dist;
     arv->cmp = cmp;
-    arv->printa = printa;
-    arv->get_key = get_key;
+    arv->exibe = exibe;
 }
 
 void insere_kd(Arv * arv, void * reg) {
@@ -26,11 +25,11 @@ void insere_node_kd(Arv * arv, Node ** node, void * reg, int eixo) {
     else insere_node_kd(arv, &((*node)->dir), reg, ++eixo % arv->dim);
 }
 
-void * busca_kd(Arv * arv, void * reg, int cod) {
+void * busca_kd(Arv * arv, void * reg) {
     return busca_node_kd(arv, arv->raiz, reg, 0);
 }
-void * busca_node_kd(Arv * arv, Node * node, void * reg, int eixo, int cod) {
-    Node * ret;
+void * busca_node_kd(Arv * arv, Node * node, void * reg, int eixo) {
+    void * ret;
 
     if(!node) ret = NULL;
     else if(arv->cmp(node->reg, reg, eixo) > 0) busca_node_kd(arv, node->esq, reg, ++eixo % arv->dim);
@@ -48,22 +47,22 @@ void libera_kd(Arv * arv) {
 
 void libera_node_kd(Node * node) {
     if(node) {
-	libera_node_kd(node->esq);
-	libera_node_kd(node->dir);
-	free(node->reg);
-	free(node);
+		libera_node_kd(node->esq);
+		libera_node_kd(node->dir);
+		free(node->reg);
+		free(node);
     }
 }
 
-void printa_kd(Arv * arv) {
-    printa_node_kd(arv, arv->raiz);
+void exibe_kd(Arv * arv) {
+    exibe_node_kd(arv, arv->raiz);
 }
 
-void printa_node_kd(Arv * arv, Node * node) {
+void exibe_node_kd(Arv * arv, Node * node) {
     if(node) {
-	printa_node_kd(arv, node->esq);
-	arv->printa(node->reg);
-	printa_node_kd(arv, node->dir);
+		exibe_node_kd(arv, node->esq);
+		arv->exibe(node->reg);
+		exibe_node_kd(arv, node->dir);
     }
 }
 
@@ -75,38 +74,34 @@ int conta_node_kd(Node * node) {
     if(node) return 1 + conta_node_kd(node->esq) + conta_node_kd(node->dir);
 }
 
-void atribui_distancias(Arv * arv, Node * node, Node * aux, float * distancias, int tam, int * i) {
+void atribui_distancias(Arv * arv, void * reg, Node * node, float * distancias, int tam, int * i) {
     if(*i < tam) {
-	if(aux) {
-	    atribui_distancias(arv, node, aux->esq, distancias, tam, i);
-	    //distancias[i] = arv->dist(node, aux);
-	    //printf("%d\n", i);
-	    distancias[*i] = 5;
-	    printf("i: %d e dist: %f\n", *i, distancias[*i]);
-	    //printf("i: %d e dist: %f\n", i, arv->dist(node, aux));
-	    (*i)++;
-	    atribui_distancias(arv, node, aux->dir, distancias, tam, i);
+	if(node) {
+	    atribui_distancias(arv, reg, node->esq, distancias, tam, i);
+
+	    if(arv->dist(reg, node->reg)) {
+		arv->exibe(node->reg);
+		distancias[*i] = arv->dist(reg, node->reg);
+		printf("i: %d e dist: %f\n", *i, distancias[*i]);
+
+		(*i)++;
+	    }
+
+	    atribui_distancias(arv, reg, node->dir, distancias, tam, i);
 	}
     }
 }
 
-void n_proximos_kd(Arv * arv, Node * node, int n) {
-    //int qtd_nodes = 1000;
-    int qtd_nodes = conta_kd(arv);
+void n_proximos_kd(Arv * arv, void * reg, int n) {
+    int qtd_nodes = conta_kd(arv)-1;
 
     float * distancias = (float *) calloc(qtd_nodes, sizeof(float));
-    //float distancias[qtd_nodes];
+    void * proximos[n];
 
     int i = 0;
-    atribui_distancias(arv, node, arv->raiz, distancias, qtd_nodes, &i);
-
-    printf("dist: %f\n", arv->dist(node, arv->raiz));
-
-    /*
-    for(float * p = distancias; p < distancias + qtd_nodes; p++) {
-		printf("%f\n", *p);		
-    }
-    */
+    atribui_distancias(arv, reg, arv->raiz, distancias, qtd_nodes, &i);
+	
+    //printf("dist: %f\n", arv->dist(reg, arv->raiz->reg));
 
     free(distancias);
 }
